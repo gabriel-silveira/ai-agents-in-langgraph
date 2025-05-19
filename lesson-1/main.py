f12b8b49e6aaf944c74bae19d31b8407899eb05d
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2025-05-19 17:56:32 -0300

    .gitignore

---
+++ lesson-1/main.py
@@ -0,0 +1,52 @@
+from dotenv import load_dotenv
+
+_ = load_dotenv()
+
+from src.agent import Agent
+from src.prompt import prompt
+from src.actions import average_dog_weight
+
+agent = Agent(prompt)
+
+while True:
+  try:
+    user_input = input("User: ")
+    
+    if user_input.strip().lower() == "exit":
+      break
+    elif user_input.strip() == "1":
+      result = agent("How much does a Bulldog weigh?")
+
+      print("Assistant:", result)
+
+      next_prompt = "Observation: {}".format(result)
+
+      next_result = agent(next_prompt)
+
+      print("Finally:", next_result)
+    elif user_input.strip() == "2":
+      result = agent("""I have 2 dogs, a border collie and a scottish terrier. What is their combined weight?""")
+
+      print("Assistant:", result)
+
+      next_prompt = "Observation: {}".format(average_dog_weight("Border Collie"))
+
+      next_result = agent(next_prompt)
+
+      print(next_result)
+
+      next_prompt = "Observation: {}".format(average_dog_weight("Scottish Terrier"))
+
+      next_result = agent(next_prompt)
+
+      print(next_result)
+
+      next_prompt = "Observation: {}".format(eval("37 + 20"))
+
+      last_result = agent(next_prompt)
+
+      print("Finally:", last_result)
+    
+    print(f"\nMessages:\n{agent.messages}")
+  except KeyboardInterrupt:
+    break
---
+++ lesson-1/src/agent.py
@@ -0,0 +1,30 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+
+class Agent:
+  def __init__(self, system=""):
+    self.system = system
+    self.messages = []
+
+    if self.system:
+      self.messages.append({"role": "system", "content": system})
+  
+  def __call__(self, message):
+    self.messages.append({"role": "user", "content": message})
+    
+    result = self.execute()
+    
+    self.messages.append({"role": "assistant", "content": result})
+    
+    return result
+  
+  def execute(self):
+    completion = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      temperature=0,
+      messages=self.messages
+    )
+    
+    return completion.choices[0].message.content
